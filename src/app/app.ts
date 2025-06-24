ef6019425c5d862fcd589727feec3bdea8726cda
cukonroberto <cukonroberto@gmail.com>
2025-06-24 22:38:35 +0200

    Calendar component basic functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Calendar } from "./components/calendar/calendar";
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Calendar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+
+
+  selectedDate: Date = new Date();
+
+  handleDateChange(date: Date): void {
+    this.selectedDate = date;
+  }
+
+
   protected title = 'task-calendar';
 }
--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { MatCalendar } from '@angular/material/datepicker';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+
+
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule, MatCalendar, MatCardModule],
   templateUrl: './calendar.html',
   styleUrl: './calendar.scss'
 })
 export class Calendar {
+
+
+  selectedDate: Date = new Date();
+
+  @Output() dateSelected = new EventEmitter<Date>();
+
+  onDateChange(date: Date): void{
+    this.selectedDate = date;
+    this.dateSelected.emit(date);
+  }
+
+
 
 }
