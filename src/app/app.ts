1bbb047c001cd4219d4739b4c7174e7d0efca083
cukonroberto <cukonroberto@gmail.com>
2025-06-24 23:17:07 +0200

    Simple UI design changes and theming

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Calendar } from "./components/calendar/calendar";
+import { Calendar } from './components/calendar/calendar';
 import { CommonModule } from '@angular/common';
-
-
+import { Task } from './models/task.model';
+import { TaskList } from './components/task-list/task-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Calendar, CommonModule],
+  imports: [RouterOutlet, Calendar, CommonModule, TaskList],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-
-
   selectedDate: Date = new Date();
+
+  tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Workout Routine Upper A',
+      date: '2025-06-24',
+      done: false,
+    },
+
+    {
+      id: 2,
+      title: 'Workout Routine Lower B',
+      date: '2025-06-25',
+      done: false,
+    },
+    { id: 3, title: 'Create new project', date: '2025-06-25', done: false },
+  ];
+
+  getTasksForSelectedDate(): Task[] {
+    const today = this.selectedDate.toISOString().split('T')[0];
+    return this.tasks.filter((task) => task.date === today);
+  }
 
   handleDateChange(date: Date): void {
     this.selectedDate = date;
   }
-
 
   protected title = 'task-calendar';
 }
--- src/app/components/task-list/task-list.ts
+++ src/app/components/task-list/task-list.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Task } from '../../models/task.model';
+import { MatCardModule } from '@angular/material/card';
+
+
+
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule],
   templateUrl: './task-list.html',
   styleUrl: './task-list.scss'
 })
 export class TaskList {
+
+@Input() tasks: Task[] = [];
 
 }
